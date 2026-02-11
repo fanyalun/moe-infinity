67ada30701ef63ec216c384f56984472fe2f3cf7
fanyalun <yalunfan33@gmail.com>
2026-02-11 23:25:37 +0800

    修改显存容量

--- examples/benchmark_qwen3_vl_moe.py
+++ examples/benchmark_qwen3_vl_moe.py
@@ -370,7 +370,7 @@ def main():
     parser.add_argument(
         "--device-memory-ratio",
         type=float,
-        default=0.0675,
+        default=0.7,
     )
     parser.add_argument(
         "--max-new-tokens", type=int, default=50
